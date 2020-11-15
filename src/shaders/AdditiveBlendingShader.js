b0be724b354b563295b7b82bed46cc5c5c73daa2
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2020-11-15 15:31:48 +0200

    Fixed eslint error

--- src/shaders/AdditiveBlendingShader.js
+++ src/shaders/AdditiveBlendingShader.js
@@ -1,4 +1,4 @@
-export default {
+const Shader = {
     uniforms: {
         tDiffuse: { value: null },
         tAdd: { value: null },
@@ -21,3 +21,5 @@ export default {
         gl_FragColor = color + add;
       }`,
 }
+
+export default Shader
