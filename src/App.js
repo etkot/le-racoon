cec06c8075795f23e3017b1aea350a24f6853db6
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2020-11-15 15:23:28 +0200

    Le raccoon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+// model by eon/sketchfab https://sketchfab.com/3d-models/elven-ranger-statue-71aec2d9f7724ae09992435ce8ff7258
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as THREE from "three"
+import React, { Suspense, useEffect, useRef, useMemo } from "react"
+import {
+    Canvas,
+    useThree,
+    useFrame,
+    extend,
+    useLoader,
+} from "react-three-fiber"
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer"
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass"
+import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass"
+import { FXAAShader } from "three/examples/jsm/shaders/FXAAShader"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { AdditiveBlendingShader, VolumetricLightShader } from "./shaders"
+
+import "./App.css"
+
+extend({ EffectComposer, RenderPass, ShaderPass })
+
+const DEFAULT_LAYER = 0
+const OCCLUSION_LAYER = 1
+
+function LeRaccoon({ layer = DEFAULT_LAYER }) {
+    const group = useRef()
+    const gltf = useLoader(GLTFLoader, "/LeRaccoon.glb")
+    console.log(gltf)
+
+    const material = useMemo(() => {
+        if (layer === DEFAULT_LAYER)
+            return new THREE.MeshStandardMaterial({
+                color: new THREE.Color("#2a2a2a"),
+                roughness: 0.2,
+                metalness: 0.9,
+            })
+        else
+            return new THREE.MeshBasicMaterial({
+                color: new THREE.Color("black"),
+            })
+    }, [layer])
+
+    useFrame(() => (group.current.rotation.y += 0.004))
+
+    return (
+        <group ref={group}>
+            <group position={[0, 4.3, 0]}>
+                <mesh
+                    material={material}
+                    layers={layer}
+                    receiveShadow
+                    castShadow
+                >
+                    <bufferGeometry
+                        attach="geometry"
+                        {...gltf.__$[2].geometry}
+                    />
+                </mesh>
+            </group>
+        </group>
+    )
 }
 
-export default App;
+function Effects() {
+    const { gl, scene, camera, size } = useThree()
+    const occlusionRenderTarget = useMemo(
+        () => new THREE.WebGLRenderTarget(),
+        []
+    )
+    const occlusionComposer = useRef()
+    const composer = useRef()
+
+    useEffect(() => {
+        occlusionComposer.current.setSize(size.width, size.height)
+        composer.current.setSize(size.width, size.height)
+    }, [size])
+
+    useFrame(() => {
+        camera.layers.set(OCCLUSION_LAYER)
+        occlusionComposer.current.render()
+        camera.layers.set(DEFAULT_LAYER)
+        composer.current.render()
+    }, 1)
+
+    return (
+        <>
+            <mesh layers={OCCLUSION_LAYER} position={[0, 4.5, -10]}>
+                <sphereBufferGeometry attach="geometry" args={[5, 32, 32]} />
+                <meshBasicMaterial attach="material" />
+            </mesh>
+            <effectComposer
+                ref={occlusionComposer}
+                args={[gl, occlusionRenderTarget]}
+                renderToScreen={false}
+            >
+                <renderPass attachArray="passes" args={[scene, camera]} />
+                <shaderPass
+                    attachArray="passes"
+                    args={[VolumetricLightShader]}
+                    needsSwap={false}
+                />
+            </effectComposer>
+            <effectComposer ref={composer} args={[gl]}>
+                <renderPass attachArray="passes" args={[scene, camera]} />
+                <shaderPass
+                    attachArray="passes"
+                    args={[AdditiveBlendingShader]}
+                    uniforms-tAdd-value={occlusionRenderTarget.texture}
+                />
+                <shaderPass
+                    attachArray="passes"
+                    args={[FXAAShader]}
+                    uniforms-resolution-value={[
+                        1 / size.width,
+                        1 / size.height,
+                    ]}
+                    renderToScreen
+                />
+            </effectComposer>
+        </>
+    )
+}
+
+const App = () => {
+    return (
+        <Canvas
+            shadowMap
+            style={{ background: "#171717" }}
+            camera={{ position: [0, 4, 8], fov: 50 }}
+            gl={{ antialias: false }}
+            onCreated={({ gl }) => {
+                gl.toneMapping = THREE.Uncharted2ToneMapping
+                gl.outputEncoding = THREE.sRGBEncoding
+            }}
+        >
+            <ambientLight intensity={0.5} />
+            <pointLight position={[0, 60, -100]} intensity={20} />
+            <pointLight position={[-50, 0, -50]} intensity={2} />
+            <spotLight
+                castShadow
+                intensity={8}
+                angle={Math.PI / 10}
+                position={[10, 10, 10]}
+                shadow-mapSize-width={2048}
+                shadow-mapSize-height={2048}
+            />
+            <mesh position={[0, 4.5, -10]}>
+                <sphereBufferGeometry attach="geometry" args={[4.9, 32, 32]} />
+                <meshBasicMaterial
+                    attach="material"
+                    transparent
+                    opacity={0.5}
+                />
+            </mesh>
+            <Suspense fallback={null}>
+                <LeRaccoon />
+                <LeRaccoon layer={OCCLUSION_LAYER} />
+            </Suspense>
+            <Effects />
+        </Canvas>
+    )
+}
+export default App
